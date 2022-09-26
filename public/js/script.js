3f3db6669f0f2c0b62d7cd02c1376e94e23d66d6
GalaBalaMR <matus.recka@gmail.com>
2022-09-26 16:25:23 +0200

    Add script for scroll

--- public/js/script.js
+++ public/js/script.js
@@ -16,4 +16,25 @@ $( document ).ready(function() {
           $('#head-nav').removeClass('fixed');}
 	});
 
+  //if scroll, add active class in nav
+  var addClassOnScroll = function () {
+    var windowTop = $(window).scrollTop();
+    $('section[id]').each(function (index, elem) {
+        var offsetTop = $(elem).offset().top;
+        var outerHeight = $(this).outerHeight(true);
+
+        if( windowTop > (offsetTop - 50) && windowTop < ( offsetTop + outerHeight)) {
+            var elemId = $(elem).attr('id');
+            $("nav ul li a.active").removeClass('active');
+            $("nav ul li a[href='#" + elemId + "']").addClass('active');
+        }
+    });
+  };
+
+  $(function () {
+      $(window).on('scroll', function () {
+          addClassOnScroll();
+      });
+  });
+
 });
